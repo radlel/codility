ed37818565723fad37e8d38b25c0f5de999c0218
Radoslaw Lelito <radoslaw.lelito@gmail.com>
2023-09-29 16:39:17 +0200

    Created project structure and added build files

---
+++ src/sample.h
@@ -0,0 +1,9 @@
+#ifndef CODILITY_SAMPLE_H
+#define CODILITY_SAMPLE_H
+
+class Sample {
+public:
+    static void printHello();
+};
+
+#endif //CODILITY_SAMPLE_H
---
+++ test/Sample_test.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "Sample.h"
+
+TEST(dummyTest, test1) {
+    //arrange
+    //act
+    //assert
+    EXPECT_EQ (0,  0);
+}
