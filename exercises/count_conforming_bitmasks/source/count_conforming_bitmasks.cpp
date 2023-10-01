fe682d98c7e4191eae0f37a1c9ec98df250be6ce
Radoslaw Lelito <radoslaw.lelito@gmail.com>
2023-10-01 22:32:05 +0200

    Added algorithm description

--- exercises/count_conforming_bitmasks/source/count_conforming_bitmasks.cpp
+++ exercises/count_conforming_bitmasks/source/count_conforming_bitmasks.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include "count_conforming_bitmasks.h"
 
-#define VALUE_LEN (30)
+
+const int VALUE_LEN_BITS = 30;
 
 
 int getNumberOfCombinations(int val) {
+    /* Returns number of conforming combinations for provided integer */
     int result = 1;
 
-    for (int i = 0; i < VALUE_LEN; i++) {
+    /* Check each bit value in provided integer, if equals 0 multiply result by 2.
+       Example: for integer 0b1110 there are 2 conforming combinations: 
+       0b1110, 0b1111 */
+    for (int i = 0; i < VALUE_LEN_BITS; i++) {
         if (0 == (val & (1 << i))) {
             result *= 2;
         }
@@ -16,7 +21,6 @@ int getNumberOfCombinations(int val) {
     return result;
 }
 
-
 int solution(int A, int B, int C) {
     int result = 0;
 
@@ -24,16 +28,22 @@ int solution(int A, int B, int C) {
     int n_a_or_b = 0, n_a_or_c = 0, n_b_or_c = 0;
     int n_a_or_b_or_c = 0;
 
+    /* Check how many conforming combinations exist for integers A, B, C */
     n_a = getNumberOfCombinations(A);
     n_b = getNumberOfCombinations(B);
     n_c = getNumberOfCombinations(C);
 
+    /* Check how many common conforming combinations exist for integer pairs A|B, A|C, B|C */
     n_a_or_b = getNumberOfCombinations(A | B);
     n_a_or_c = getNumberOfCombinations(A | C);
     n_b_or_c = getNumberOfCombinations(B | C);
 
+    /* Check how many common conforming combinations exist for integer group A|B|C */
     n_a_or_b_or_c = getNumberOfCombinations(A | B | C);
 
+    /* Sum up combinations for integers A, B and C. Then substract number of common combinations
+       of pairs. Also if there are combinations conforming whole group, they were substracted 3
+       times in previous step and are not considered anymore. Add them back. */
     result = n_a + n_b + n_c - (n_a_or_b + n_a_or_c + n_b_or_c) + n_a_or_b_or_c;
 
     return result;
